a4d599cafa6caafbec9f33a4e95c8e6e361671f7
Mrinalini <upadhya.mrinalini@gmail.com>
2019-04-18 21:09:56 -0400

    Action tflite file added

--- openCVLibrary342/build/generated/aidl_source_output_dir/debug/compileDebugAidl/out/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary342/build/generated/aidl_source_output_dir/debug/compileDebugAidl/out/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /home/mrinalini/Documents/Project/PoseWithKotlin/openCVLibrary342/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /home/mrinalini/Documents/Project/Current/Human-Activity-Recognition/openCVLibrary342/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
